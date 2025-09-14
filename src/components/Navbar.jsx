295750a59acc5cac378749b939c18b7408a36cb7
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-09-14 23:49:24 +0530

    update icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,56 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-   <header className="text-gray-600 body-font ">
-  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg>
-      <span className="ml-3 text-xl font-bold">Scriptify</span>
-    </a>
-    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-      <a className="mr-5 hover:text-gray-900 cursor-pointer text-xl">Home</a>
-      <a className="mr-5 hover:text-gray-900 cursor-pointer text-xl">About</a>
-      <a className="mr-5 hover:text-gray-900 cursor-pointer text-xl">Products</a>
-      <a className="mr-5 hover:text-gray-900 cursor-pointer text-xl">Contact</a>
-    </nav>
-    <button className=" border-2 inline-flex items-center bg-gray-100 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-gray-600 mt-4 md:mt-0">Log In
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-        <path d="M5 12h14M12 5l7 7-7 7"></path>
-      </svg>
-    </button>
-  </div>
-</header>
-  )
-}
+    <header className="text-gray-600 body-font ">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="white"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="w-10 h-10 p-2 bg-indigo-500 rounded-full"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2l3.09 6.26L22 9.27l-5 4.87L18.18 22 12 18.56 5.82 22 7 14.14l-5-4.87 6.91-1.01L12 2z" />
+          </svg>
 
-export default Navbar
+          <span className="ml-3 text-xl font-bold">Scriptify</span>
+        </a>
+        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+          <a className="mr-5 hover:text-gray-900 cursor-pointer text-xl">
+            Home
+          </a>
+          <a className="mr-5 hover:text-gray-900 cursor-pointer text-xl">
+            About
+          </a>
+          <a className="mr-5 hover:text-gray-900 cursor-pointer text-xl">
+            Products
+          </a>
+          <a className="mr-5 hover:text-gray-900 cursor-pointer text-xl">
+            Contact
+          </a>
+        </nav>
+        <button className=" border-2 inline-flex items-center bg-gray-100 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-gray-600 mt-4 md:mt-0">
+          Log In
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            class="w-4 h-4 ml-1"
+            viewBox="0 0 24 24"
+          >
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
